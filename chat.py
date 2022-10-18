cc512b013165d2a0574375dd6753b4a09d1f7983
Harry-Wieland <hlwst796@mail.rmu.edu>
2022-10-18 15:27:41 -0400

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,6 +1,9 @@
 import threading
+import os
+import time
 import socket
 import sys
+from random import randint
 
 class Server:
     clients = []
@@ -12,22 +15,24 @@ class Server:
         server.listen()
         while True:
             client, address = server.accept()
-            print(f"Connected with {str(address)}")
+            
 
     #Sends the client a keyword so user knows to send a nickname
-            client.send('NICK'.encode('ascii'))
-            nickname = client.recv(1024).decode('ascii')
+            client.send('NICK'.encode('utf-8'))
+            nickname = client.recv(1024).decode('utf-8')
             self.nicknames.append(nickname)
             self.clients.append(client)
-            self.peers.append(address)
+            self.peers.append(address[0])
             self.sendPeers()
+            time.sleep(.1)
 
     #Lets everyone know who joined the chat and the particular client it worked
-            self.broadcast(f'{nickname} joined the chat' .encode('ascii'))
-            client.send('Connected to the server!' .encode('ascii'))
-
+            client.send('Connected to the server!' .encode('utf-8'))
+            self.broadcast(f'{nickname} joined the chat' .encode('utf-8'))
+            
             thread = threading.Thread(target=self.handle, args=(client, address))
             thread.start()
+
 
     def broadcast(self, message):
         for client in self.clients:
@@ -43,26 +48,26 @@ class Server:
                 self.clients.remove(client)
                 client.close()
                 nickname = self.nicknames[index]
-                self.broadcast(f'{nickname} left the chat' .encode('ascii'))
+                self.broadcast(f'{nickname} left the chat' .encode('utf-8'))
                 self.nicknames.remove(nickname)
-                self.peers.remove(address)
+                self.peers.remove(address[0])
                 self.sendPeers()
                 break
     def sendPeers(self):
         p = ""
         for peer in self.peers:
             p = p + peer + ","
-        for conncection in self.clients:
-            conncection.send(b'\x11' + bytes(p, "ascii"))
-
+        for client in self.clients:
+            client.send(b'\x11' + p.encode('utf-8'))
 
 class Client:
-    nickname = ""
+
     
     def __init__(self, address):
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect((address, 55555))
-        self.nickname = input("Choose a nickname ")
+        if connceted.nickname == "":
+            connceted.nickname = input("Choose a nickname ")
         receive_thread = threading.Thread(target=self.receive, args=(client,))
         receive_thread.start()
 
@@ -72,23 +77,65 @@ class Client:
     def receive(self, client):
         while True:
             try:
-                message = client.recv(1024).decode('ascii')
-                if message == 'NICK':
-                    client.send(self.nickname.encode('ascii'))
+                message = client.recv(1024)
+                if message[0:1] == b'':
+                    print("An error occurred!")
+                    connceted.connected = False
+                    client.close()
+                    break
+                if str(message, 'utf-8') == 'NICK':
+                    client.send(connceted.nickname.encode('utf-8'))
+                elif message[0:1] == b'\x11':
+                    self.updatePeers(message[1:])
                 else:
-                    print(message)
+                    print(str(message, 'utf-8'))
             except:
                 print("An error occurred!")
+                connceted.connected = False
                 client.close()
                 break
 
     def write(self, client):
         while True:
-            message = f'{self.nickname}: {input("")}'
-            client.send(message.encode('ascii'))
+            message = f'{connceted.nickname}: {input("")}'
+            client.send(message.encode('utf-8'))
+    def updatePeers(self, peerData):
+        p2p.peers = str(peerData, 'utf-8'). split(",")[:-1]
 
 
-if (len(sys.argv) > 1):
-    pass
-else:
-    server = Server()
+class p2p:
+    peers = ['127.0.0.1']
+class connceted:
+    connected = False
+    nickname = ""
+
+
+if (len(sys.argv) == 2):
+    pid=os.fork()
+    if pid:
+        server = Server()
+    else:
+        connceted.connected = True
+        client = Client(p2p.peers[0])
+if (len(sys.argv) > 2):
+    connceted.connected = True
+    client = Client(sys.argv[2])
+while True:
+    try:
+        if connceted.connected == False:
+            print("Connecting")
+            time.sleep(randint(1, 5))
+            for peer in p2p.peers:
+                if connceted.connected == False:
+                    try:
+                        client = Client(peer)
+                        connceted.connected = True
+                    except:
+                        pass
+                    if connceted.connected == False:
+                        try:
+                            server = Server()
+                        except:
+                            print("server no start")
+    except:
+        pass
